e3e9666ab20c0634e8e825f89e561bc65a39bdd2
Adam Stortz <adam.stortz@live.com>
2023-05-16 14:50:38 -0500

    feat: add nginx sample service

--- infastructure/workload/main.tf
+++ infastructure/workload/main.tf
@@ -77,3 +77,62 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+
+resource "kubernetes_namespace" "workload" {
+  metadata {
+    name = local.workload_name
+  }
+}
+resource "kubernetes_deployment" "workload" {
+  metadata {
+    name      = local.service_name
+    namespace = kubernetes_namespace.workload.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = local.service_name
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = local.service_name
+        }
+      }
+      spec {
+        container {
+          image = "nginx"
+          name  = "nginx-container"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "workload" {
+  metadata {
+    name      = local.service_name
+    namespace = kubernetes_namespace.workload.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.workload.spec.0.template.0.metadata.0.labels.app
+    }
+    type = "LoadBalancer"
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
