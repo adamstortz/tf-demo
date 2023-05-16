d27f45925d59e821b6aa96b9dd133d331b18f974
Adam Stortz <adam.stortz@live.com>
2023-05-16 15:20:41 -0500

    fix: revert change that should not have been committed

--- infastructure/workload/outputs.tf
+++ infastructure/workload/outputs.tf
@@ -7,8 +7,3 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
-
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = kubernetes_service.workload.
-}
