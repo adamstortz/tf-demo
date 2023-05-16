e41841035ef3b53df81c8c56dd1bee0b6236c1b2
Adam Stortz <adam.stortz@live.com>
2023-05-16 15:16:52 -0500

    fix: fixes cluster rbac that blocks github

--- infastructure/workload/outputs.tf
+++ infastructure/workload/outputs.tf
@@ -1,9 +1,14 @@
-# output "cluster_endpoint" {
-#   description = "Kubernetes Cluster Endpoint"
-#   value       = module.eks.cluster_endpoint
-# }
+output "cluster_endpoint" {
+  description = "Kubernetes Cluster Endpoint"
+  value       = module.eks.cluster_endpoint
+}
 
-# output "cluster_name" {
-#   description = "Kubernetes Cluster Name"
-#   value       = module.eks.cluster_name
-# }
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = kubernetes_service.workload.
+}
