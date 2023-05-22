0364ab2f180d733182830914c5ef2ca808a48bb8
Adam Stortz <adam.stortz@live.com>
2023-05-22 15:32:15 -0500

    feat: implement dynamic timestamp (#8)

--- service/test/unit/service.spec.ts
+++ service/test/unit/service.spec.ts
@@ -4,9 +4,9 @@ const service = require("../../src");
 describe("Service unit tests", () => {
   it("Should have expected payload", () => {
     const result = service.getResponseBody();
-
+    console.log(result)
     expect(result.message).to.eql('Automate all the things!');
     expect(result.timestamp).to.be.a('number');
-    expect(result.timestamp).to.be.within(1529729123, 1700000000);
+    expect(result.timestamp).to.be.within(1600000000000, 2000000000000);
   });
 });
